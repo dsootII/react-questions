5fb22d7b13679d267dcd9ffd19e0e428b9b978b7
Kunal Kamtikar <kunalkamtikar@Kunals-MacBook-Pro.local>
2024-06-21 23:49:15 +0530

    finished drag and drop

--- allQuestions/allQuestions/src/DragAndDrop/DragAndDrop.jsx
+++ allQuestions/allQuestions/src/DragAndDrop/DragAndDrop.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { firstList, secondList } from './listData';
 
 export default function DragAndDrop() {
 
-  function handleDragStart (e) {
-    e.dataTransfer.setData("Text", e.target.id);
+  const [list1, setList1] = useState(firstList);
+  const [list2, setList2] = useState(secondList);
+  const dragRef = useRef(null);
+
+  function handleDragStart(e) {
+    dragRef.current = e.target.id;
   }
 
-  function handleDropAllowance (e) {
-    e.preventDefault();
+  function handleDropIntoList1(e) {
+    setList2(list2.filter(listItem => listItem !== dragRef.current));
+    setList1([...list1, dragRef.current]);
   }
 
-  function handleDrop (e) {
-    let data = e.dataTransfer.getData("Text")
-
+  function handleDropIntoList2(e) {
+    setList1(list1.filter(listItem => listItem !== dragRef.current));
+    setList2([...list2, dragRef.current]);
   }
 
   //dragging is fine but after that we gotta use state. We'll have two states, list1 and list 2
@@ -20,20 +26,40 @@ export default function DragAndDrop() {
   return (
     <div className='px-10'>
       <div>list 1:</div>
-      <ul className='list-disc'>
-        <li draggable="true">l1i1</li>
-        <li draggable="true">l1i2</li>
-        <li draggable="true">l1i3</li>
-        <li draggable="true">l1i4</li>
-        <li draggable="true">l1i5</li>
+      <ul className='list-disc' id='list1'>
+        {
+          list1.map((list1Item) => {
+            return <li 
+            draggable="true" 
+            key={list1Item} 
+            id={list1Item} 
+            value={list1Item} 
+            onDragStart={(e) => handleDragStart(e)}
+            onDragOver={e => e.preventDefault()}
+            onDrop={(e) => handleDropIntoList1(e)}
+            >
+              {list1Item}
+            </li>
+          })
+        }
       </ul>
       <div>list 2:</div>
       <ul className='list-disc'>
-        <li draggable="true">l2i1</li>
-        <li draggable="true">l2i2</li>
-        <li draggable="true">l2i3</li>
-        <li draggable="true">l2i4</li>
-        <li draggable="true">l2i5</li>
+        {
+          list2.map((list2Item) => {
+            return <li 
+            draggable="true" 
+            key={list2Item} 
+            id={list2Item} 
+            value={list2Item}
+            onDragStart={(e) => handleDragStart(e)}
+            onDragOver={e => e.preventDefault()}
+            onDrop={(e) => handleDropIntoList2(e)}
+            >
+              {list2Item}
+            </li>
+          })
+        }
       </ul>
     </div>
   )
---
+++ allQuestions/allQuestions/src/DragAndDrop/listData.js
@@ -0,0 +1,15 @@
+export const firstList = [
+    'l1i1',
+    'l1i2',
+    'l1i3',
+    'l1i4',
+    'l1i5'
+]
+
+export const secondList = [
+    'l2i1',
+    'l2i2',
+    'l2i3',
+    'l2i4',
+    'l2i5'
+]
